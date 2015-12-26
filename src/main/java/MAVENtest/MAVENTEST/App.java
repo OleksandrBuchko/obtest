887fa9e93c660b6a1e64bc854e9dee583835b35b
OleksandrBuchko <oleksandr.buchko@gmail.com>
2015-12-26 23:53:57 +0200

    first commit

---
+++ src/main/java/MAVENtest/MAVENTEST/App.java
@@ -0,0 +1,13 @@
+package MAVENtest.MAVENTEST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
